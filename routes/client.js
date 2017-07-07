f2c79c981741c96114f6d52ef903bac0a30a3080
Suraj Poddar <surajpoddar16@gmail.com>
2017-07-07 19:25:25 +0530

    Get Avatar api created and API errorhandler added

--- routes/client.js
+++ routes/client.js
@@ -2,11 +2,14 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+var user = require('../main-modules/user');
 
 // Route definations
 router.get('/', (req, res) => {
   res.sendFile(path.normalize(__dirname + '/../views/index.html'));
 });
+
+router.get('/user/avatar', user.getAvatar);
 
 // Exported values
 module.exports = router;
